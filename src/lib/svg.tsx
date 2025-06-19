73ea0af500e7eda53977cd81a3a03ea02abaa8f4
John Tobi <johnriad.dev@gmail.com>
2025-06-19 13:24:54 -0300

    fix: removed Usememo from svg.tsx

--- src/lib/svg.tsx
+++ src/lib/svg.tsx
@@ -1,15 +1,9 @@
 import { SVGLoader } from "three/addons/loaders/SVGLoader.js"
-import {useMemo} from "react"
 
 export const loadSvg = (svg: string) => {
     const loader = new SVGLoader()
-    const shapes=useMemo(()=>{  
     const {paths} = loader.parse(svg)
-
     return paths.flatMap(path => path.toShapes(true))
-},[svg])
-
-return shapes
 }
 
 
