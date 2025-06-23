9a286ea1b0ca1819e2c595cb27ce107f8d107b6c
John Tobi <johnriad.dev@gmail.com>
2025-06-23 01:47:15 -0300

    feat: added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { SVGProvider } from "./context/svgcontext";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* async external loader */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XSN9QDJG7J`}
+          strategy="afterInteractive"
+        />
+        {/* inline config */}
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XSN9QDJG7J', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
